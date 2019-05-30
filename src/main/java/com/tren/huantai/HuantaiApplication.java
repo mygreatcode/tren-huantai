297a2e8e9fbe39f1ed5bd8ac41719fefd959e4f7
mygreatcode <599821922@qq.com>
2019-05-31 02:18:45 +0800

    first commit the project @committer:tren date:2019-05-31

---
+++ src/main/java/com/tren/huantai/HuantaiApplication.java
@@ -0,0 +1,13 @@
+package com.tren.huantai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuantaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuantaiApplication.class, args);
+    }
+
+}
